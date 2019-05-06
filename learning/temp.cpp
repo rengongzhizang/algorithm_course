99665d038464fa60a60f0a39c7356c034dec56a0
Yuanhang Cui <cuiyuanhang@sais-MacBook-puro.local>
2019-05-06 18:16:30 +0800

    zigzag

--- learning/temp.cpp
+++ learning/temp.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <queue>
 #include <functional>
+#include <deque>
 using namespace std;
 struct TreeNode {
     int val;
@@ -32,12 +33,90 @@ void print_tree(TreeNode* root, vector<vector<int> >& res) {
         res.push_back(sol);
     }
 }
-vector<vector<int> > print_tree_by_layer(TreeNode* root) {
+void zigzag_print_tree(TreeNode* root, vector<vector<int> >& res) {
+    if (root == NULL) {
+        return;
+    }
+    queue<TreeNode*> q;
+    q.push(root);
+    bool flag = 0;
+    while (!q.empty()) {
+        int size = q.size();
+        vector<int> sol;
+        for (int i = 0; i < size; i++) {
+            if (q.front() -> left != NULL) {
+                q.push(q.front() -> left);
+            }
+            if (q.front() -> right != NULL) {
+                q.push(q.front() -> right);
+            }
+            sol.push_back(q.front() -> val);
+            q.pop();
+        }
+        if (flag) {
+            reverse(sol.begin(), sol.end());
+        }
+        flag = !flag;
+        res.push_back(sol);
+    }
+}
+
+pair<vector<vector<int> >, vector<vector<int> > > print_tree_by_layer(TreeNode* root) {
+    if (root == NULL) {
+        return pair<vector<vector<int> >, vector<vector<int> > >();
+    }
+    vector<vector<int> > res;
+    vector<vector<int> > ans;
+    print_tree(root, res);
+    zigzag_print_tree(root, ans);
+    pair<vector<vector<int> >, vector<vector<int> > > vec_pair(res, ans);
+    return vec_pair;
+}
+
+void zigzag_print(TreeNode* root, vector<vector<int> >& ans) {
+    if (root == NULL) {
+        return;
+    }
+    deque<TreeNode*> dq;
+    dq.push_back(root);
+    bool flag = 1;
+    while (!dq.empty()) {
+        int size = dq.size();
+        vector<int> sol;
+        for (int i = 0; i < size; i++) {
+            TreeNode* curr = NULL;
+            if (flag) {
+                curr = dq.front();
+                dq.pop_front();
+                if (curr -> left != NULL) {
+                    dq.push_back(curr -> left);
+                }
+                if (curr -> right != NULL) {
+                    dq.push_back(curr -> right);
+                }
+            } else {
+                curr = dq.back();
+                dq.pop_back();
+                if (curr -> right != NULL) {
+                    dq.push_front(curr -> right);
+                }
+                if (curr -> left != NULL) {
+                    dq.push_front(curr -> left);
+                }
+            }
+            sol.push_back(curr -> val);
+        }
+        ans.push_back(sol);
+        flag = !flag;
+    }
+}
+
+vector<vector<int> > zigzag_print(TreeNode* root) {
     if (root == NULL) {
         return vector<vector<int> >();
     }
     vector<vector<int> > res;
-    print_tree(root, res);
+    zigzag_print(root, res);
     return res;
 }
 void print_vec(vector<int>& vec) {
@@ -64,10 +143,25 @@ int main() {
     TreeNode* root = new TreeNode(1);
     TreeNode* node1 = new TreeNode(2);
     TreeNode* node2 = new TreeNode(3);
+    TreeNode* node3 = new TreeNode(4);
+    TreeNode* node4 = new TreeNode(5);
+    TreeNode* node5 = new TreeNode(6);
     root -> left = node1;
     node1 -> left = node2;
-    vector<vector<int> > res = print_tree_by_layer(root);
+    node1 -> right = node3;
+    root -> right = node4;
+    node4 -> right = node5;
+    pair<vector<vector<int> >, vector<vector<int> > > pair = print_tree_by_layer(root);
+    vector<vector<int> > res = pair.first;
+    vector<vector<int> > ans = pair.second;
+    vector<vector<int> > sol = zigzag_print(root);
     for (auto& i : res) {
+        print_vec(i);
+    }
+    for (auto& i : ans) {
+        print_vec(i);
+    }
+    for (auto& i : sol) {
         print_vec(i);
     }
     cout<< '\n' <<"life is lonly" << '\n';
